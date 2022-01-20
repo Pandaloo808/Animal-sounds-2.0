4b8c4251da5110611a2124edaeafc53028298a4c
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2022-01-20 15:48:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function startClassification(){
 navigator.mediaDevices.getUserMedia({audio:true});
-  classifier=ml5.soundClassifier('',modelReady);
+  classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/lnOIbDsYb/',modelReady);
   }
 function modelReady(){
 classifier.classify(gotResults);
